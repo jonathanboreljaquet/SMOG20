9f3d52a1854a8a5d841b3656785df30842d0609a
jonathanboreljaquet <jonathan.brljq@eduge.ch>
2020-10-01 14:21:48 +0200

    Creation of the basic model of buildings scene

--- src/ts/scenes/buildings_scene.ts
+++ src/ts/scenes/buildings_scene.ts
@@ -2,6 +2,7 @@ import * as BABYLON from 'babylonjs';
 import Scene from './scene';
 import FloorsScene from './floors_scene';
 import { Vector2, Vector3 } from 'babylonjs';
+import { GridMaterial } from "babylonjs-materials";
 
 
 // Constants
@@ -11,9 +12,9 @@ const SCENE_DEFAULT_BACKCOLOR: BABYLON.Color4 = new BABYLON.Color4(0, 0, 0, 1);
 const CAMERA_UPPER_LIMIT: number = Math.PI / 2.2;
 const CAMERA_MIN_RADIUS: number = 200;
 const CAMERA_MAX_RADIUS: number = 8;
-const CAMERA_DEFAULT_ALPHA: number = 0;//Math.PI / 1.5;
-const CAMERA_DEFAULT_BETA: number = 0;// Math.PI / 2.4;
-const CAMERA_DEFAULT_RADIUS: number = 10;
+const CAMERA_DEFAULT_ALPHA: number = 0;
+const CAMERA_DEFAULT_BETA: number = Math.PI / 2.4;
+const CAMERA_DEFAULT_RADIUS: number = 20;
 
 export default class BuildingsScene extends Scene{
     // Fields
@@ -34,20 +35,42 @@ export default class BuildingsScene extends Scene{
             this.scene
         );
         this.camera.attachControl(canvas, true);
+        (this.camera.inputs.attached.pointers as BABYLON.ArcRotateCameraPointersInput).buttons = [0];
         this.camera.upperBetaLimit = CAMERA_UPPER_LIMIT;
         this.camera.lowerRadiusLimit = CAMERA_MAX_RADIUS;
         this.camera.upperRadiusLimit = CAMERA_MIN_RADIUS;
 
         this.light = new BABYLON.HemisphericLight('plan_scene_light', new BABYLON.Vector3(0, 1, 0), this.scene);
-
-        BABYLON.SceneLoader.ImportMesh("", "../../../public/mesh/cfpthorlogerie.babylon", "", this.scene, (meshes)=>  {
-            meshes[0].scaling = new Vector3(0.1,0.1,0.1);
+        
+        BABYLON.SceneLoader.ImportMesh("", "../../../public/mesh/cfptrhonetest.babylon", "", this.scene, (meshes)=>  {
+            meshes[0].scaling = new Vector3(1,1,1);
             meshes[0].actionManager = new BABYLON.ActionManager(this.scene);
+            meshes[0].position =new Vector3(0,1,0);
             meshes[0].actionManager.registerAction(new BABYLON.ExecuteCodeAction(BABYLON.ActionManager.OnPickUpTrigger, () => {
-            this.changeScene(new FloorsScene(engine, canvas));
-            meshes[0].position = BABYLON.Vector3.Zero();
-        }));
+                this.changeScene(new FloorsScene(engine, canvas));
+            }));
         });
+        BABYLON.SceneLoader.ImportMesh("", "../../../public/mesh/cfpthorlogerietest.babylon", "", this.scene, (meshes)=>  {
+            meshes[0].scaling = new Vector3(1,1,1);
+            meshes[0].actionManager = new BABYLON.ActionManager(this.scene);
+            meshes[0].position = new Vector3(0,1,-10);
+            meshes[0].actionManager.registerAction(new BABYLON.ExecuteCodeAction(BABYLON.ActionManager.OnPickUpTrigger, () => {
+                this.changeScene(new FloorsScene(engine, canvas));
+            }));
+        });
+        BABYLON.SceneLoader.ImportMesh("", "../../../public/mesh/cfptterniertest.babylon", "", this.scene, (meshes)=>  {
+            meshes[0].scaling = new Vector3(1,1,1);
+            meshes[0].actionManager = new BABYLON.ActionManager(this.scene);
+            meshes[0].position = new Vector3(0,1,10);
+            meshes[0].actionManager.registerAction(new BABYLON.ExecuteCodeAction(BABYLON.ActionManager.OnPickUpTrigger, () => {
+                this.changeScene(new FloorsScene(engine, canvas));
+            }));
+        });
+        var material = new GridMaterial("grid", this.scene);
+        var ground = BABYLON.Mesh.CreateGround("ground1", 100, 100, 2,this.scene);
+
+
+        ground.material = material;
         
         
     }
