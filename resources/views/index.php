f0993217282cbb436fcbed57672f5bbecbeec29c
lorenzo.bdcc <lorenzo1227@gmail.com>
2020-10-08 16:10:51 +0200

    Remplacement de Bootsrap par Bulma et mise en place de la navbar dynamique

--- resources/views/index.php
+++ resources/views/index.php
@@ -6,20 +6,58 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>SMOG20</title>
     <link rel="stylesheet" href="style.css">
+
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.1/css/bulma.min.css">
+
 </head>
 
 <body>
-    <div class="sidebar">
-        <a class="active" id="btnHome">Buildings</a>
-        <a id="sidebar_link_floors_scene">Floors</a>
-        <a></a>
-        <a id="sidebar_link_floors_scene">Ternier</a>
-        <a id="sidebar_link_floors_scene">Rhone</a>
-        <a id="sidebar_link_floors_scene">Butin</a>
-    </div>
+
+    <nav class="navbar is-dark" role="navigation" aria-label="main navigation">
+        <div class="navbar-brand">
+            <a class="navbar-item" href="https://bulma.io" id="sidebar_link_floors_scene">
+                <h1 class="is-1">Smog 20</h1>
+            </a>
+
+            <a role="button" class="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+                <span aria-hidden="true"></span>
+                <span aria-hidden="true"></span>
+                <span aria-hidden="true"></span>
+            </a>
+        </div>
+
+        <div class="navbar-menu" id="btnHome">
+            <div class="navbar-start">
+                <a class="navbar-item">
+                    Home
+                </a>
+
+                <div class="navbar-item has-dropdown is-hoverable">
+                    <a class="navbar-link">
+                        Batiments
+                    </a>
+                    <div class="navbar-dropdown" id="link_batiment">
+
+                    </div>
+                </div>
+            </div>
+
+
+        </div>
+    </nav>
+
+
+
     <canvas id="renderCanvas"></canvas>
     <script src="bundle.js"></script>
 
+
+
+
+    <script defer src="https://use.fontawesome.com/releases/v5.14.0/js/all.js"></script>
 </body>
 
 </html>
--- src/ts/index.ts
+++ src/ts/index.ts
@@ -1,5 +1,34 @@
 import "../scss/style.scss";
 import Engine from "./libraries/engine";
+import axios from "axios";
+import ENV from "./environnement";
 
 let canvas = document.getElementById("renderCanvas") as HTMLCanvasElement;
 let engine = new Engine(canvas);
+getFloors(1);
+function getFloors(building_id: number): Promise<any> {
+    return new Promise<any>((resolve, reject) => {
+        axios
+            .post(ENV.API_ENDPOINT + "buildings", {
+                building_id: building_id,
+            })
+            .then(
+                (response) => {
+                    resolve(response.data);
+                    console.log(response);
+                    let html = "";
+                    for (let index = 0; index < response.data.length; index++) {
+                        html +=
+                            '<a class="dropdown-item" href = "#" > ' +
+                            response.data[index].name +
+                            " </a>";
+                    }
+                    document.getElementById("link_batiment").innerHTML = html;
+                },
+                (error) => {
+                    reject(error);
+                }
+            )
+            .finally();
+    });
+}
