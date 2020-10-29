98e275539eda11521641142b599f96c91d375517
lorenzo.bdcc <lorenzo1227@gmail.com>
2020-10-29 16:06:24 +0100

    test d'affichage des horaires

--- resources/views/index.php
+++ resources/views/index.php
@@ -65,10 +65,114 @@
             </header>
             <section class="modal-card-body">
                 <div>
-                    <ul id="Schedule_ul">
-
-                    </ul>
-
+                    <table class="darkTable">
+                        <thead>
+                            <tr>
+                                <th>head1</th>
+                                <th>head2</th>
+                                <th>head3</th>
+                                <th>head4</th>
+                                <th>head5</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>cell1_1</td>
+                                <td>cell2_1</td>
+                                <td>cell3_1</td>
+                                <td>cell4_1</td>
+                                <td>cell5_1</td>
+                            </tr>
+                            <tr>
+                                <td>cell1_2</td>
+                                <td>cell2_2</td>
+                                <td>cell3_2</td>
+                                <td>cell4_2</td>
+                                <td>cell5_2</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                    <table class="darkTable">
+                        <thead>
+                            <tr>
+                                <th>head1</th>
+                                <th>head2</th>
+                                <th>head3</th>
+                                <th>head4</th>
+                                <th>head5</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>cell1_1</td>
+                                <td>cell2_1</td>
+                                <td>cell3_1</td>
+                                <td>cell4_1</td>
+                                <td>cell5_1</td>
+                            </tr>
+                            <tr>
+                                <td>cell1_2</td>
+                                <td>cell2_2</td>
+                                <td>cell3_2</td>
+                                <td>cell4_2</td>
+                                <td>cell5_2</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                    <table class="darkTable">
+                        <thead>
+                            <tr>
+                                <th>head1</th>
+                                <th>head2</th>
+                                <th>head3</th>
+                                <th>head4</th>
+                                <th>head5</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>cell1_1</td>
+                                <td>cell2_1</td>
+                                <td>cell3_1</td>
+                                <td>cell4_1</td>
+                                <td>cell5_1</td>
+                            </tr>
+                            <tr>
+                                <td>cell1_2</td>
+                                <td>cell2_2</td>
+                                <td>cell3_2</td>
+                                <td>cell4_2</td>
+                                <td>cell5_2</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                    <table class="darkTable">
+                        <thead>
+                            <tr>
+                                <th>head1</th>
+                                <th>head2</th>
+                                <th>head3</th>
+                                <th>head4</th>
+                                <th>head5</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>cell1_1</td>
+                                <td>cell2_1</td>
+                                <td>cell3_1</td>
+                                <td>cell4_1</td>
+                                <td>cell5_1</td>
+                            </tr>
+                            <tr>
+                                <td>cell1_2</td>
+                                <td>cell2_2</td>
+                                <td>cell3_2</td>
+                                <td>cell4_2</td>
+                                <td>cell5_2</td>
+                            </tr>
+                        </tbody>
+                    </table>
                 </div>
             </section>
             <footer class="modal-card-foot">
@@ -76,6 +180,9 @@
                 <button class="button" onclick="closeModal()">Cancel</button>
             </footer>
         </div>
+    </div>
+    <div>
+
     </div>
     <script>
         function closeModal() {
@@ -89,5 +196,63 @@
         }
     </script>
 </body>
+<style>
+    table.darkTable {
+        font-family: "Arial Black", Gadget, sans-serif;
+        border: 2px solid #000000;
+        background-color: #4A4A4A;
+        width: 100%;
+        height: 200px;
+        text-align: center;
+        border-collapse: collapse;
+    }
+
+    table.darkTable td,
+    table.darkTable th {
+        border: 1px solid #4A4A4A;
+        padding: 3px 2px;
+    }
+
+    table.darkTable tbody td {
+        font-size: 13px;
+        color: #E6E6E6;
+    }
+
+    table.darkTable tr:nth-child(even) {
+        background: #888888;
+    }
+
+    table.darkTable thead {
+        background: #000000;
+        border-bottom: 3px solid #000000;
+    }
+
+    table.darkTable thead th {
+        font-size: 15px;
+        font-weight: bold;
+        color: #E6E6E6;
+        text-align: center;
+        border-left: 2px solid #4A4A4A;
+    }
+
+    table.darkTable thead th:first-child {
+        border-left: none;
+    }
+
+    table.darkTable tfoot {
+        font-size: 12px;
+        font-weight: bold;
+        color: #E6E6E6;
+        background: #000000;
+        background: -moz-linear-gradient(top, #404040 0%, #191919 66%, #000000 100%);
+        background: -webkit-linear-gradient(top, #404040 0%, #191919 66%, #000000 100%);
+        background: linear-gradient(to bottom, #404040 0%, #191919 66%, #000000 100%);
+        border-top: 1px solid #4A4A4A;
+    }
+
+    table.darkTable tfoot td {
+        font-size: 12px;
+    }
+</style>
 
 </html>
